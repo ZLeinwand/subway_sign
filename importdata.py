e7054bce05c7aa7a4f81dccad82850be31c49e7e
kevinmcalear <kevin.mcalear@gmail.com>
2018-03-18 19:29:30 -0400

    more updates to led matrix command

--- importdata.py
+++ importdata.py
@@ -46,7 +46,7 @@ while True:
 			times = []
 			out = ''
 
-			os.system('sudo ./rpi-rgb-led-matrix/examples-api-use/demo -r 16 -c 2 -t 5 -b 50 -D 1 -m 5000 dynamicimages/dynamictime.ppm --led-no-hardware-pulse --led-gpio-mapping=adafruit-hat')
+			os.system('sudo ./rpi-rgb-led-matrix/examples-api-use/demo --led-chain=4 -t 5 -b 50 -D 1 -m 5000 dynamicimages/dynamictime.ppm --led-no-hardware-pulse --led-gpio-mapping=adafruit-hat')
 	except Exception:
 		print traceback.format_exc()
 
