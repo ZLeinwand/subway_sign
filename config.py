e0d0285fccd94bd4318a6020ba78e61991967446
kevinmcalear <kevin.mcalear@gmail.com>
2018-03-18 19:32:39 -0400

    fixes merge conflicts

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 
 MTA_KEY = os.environ["MTA_API_KEY"] #obtain one at http://web.mta.info/developers/developer-data-terms.html
 NUM_TRAINS = 3  #the number of trains to display for each station/direction combination
-STOP_IDS = ['F24']  #an array of stations/directions that you would like displayed. Find these in the stations file in staticdata
+STOP_IDS = ['F24N', 'F24', '2F24S']  #an array of stations/directions that you would like displayed. Find these in the stations file in staticdata
