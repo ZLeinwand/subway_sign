950db129ff0cda6f7b20fd34180c37be5f79a433
Sarah <sarah.mcalear@gmail.com>
2018-03-18 21:41:02 -0400

    refactor importdata to use 7 ave stop

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 
 MTA_KEY = os.environ["MTA_API_KEY"] #obtain one at http://web.mta.info/developers/developer-data-terms.html
-NUM_TRAINS = 3  #the number of trains to display for each station/direction combination
-STOP_IDS = ['230N', '419N', '230S']  #an array of stations/directions that you would like displayed. Find these in the stations file in staticdata
+NUM_TRAINS = 2  #the number of trains to display for each station/direction combination
+STOP_IDS = ['F24N', 'F24', 'F24S']  #an array of stations/directions that you would like displayed. Find these in the stations file in staticdata
--- importdata.py
+++ importdata.py
@@ -1,5 +1,6 @@
 from PIL import Image, ImageFont, ImageDraw
 from google.transit import gtfs_realtime_pb2
+import nyct_subway_pb2
 import urllib
 import datetime
 import math
@@ -7,42 +8,42 @@ import os
 from config import *
 import traceback
 
-times = []
-out = ''
+departure_times = []
+train_times = ''
 
 while True:
+
 	try:
 		mtafeed = gtfs_realtime_pb2.FeedMessage()
-		response = urllib.urlopen('http://datamine.mta.info/mta_esi.php?key=' + MTA_KEY + '&feed_id=1')
-		mtafeed.ParseFromString(response.read())
+		response = urllib.urlopen('http://datamine.mta.info/mta_esi.php?key=' + MTA_KEY + '&feed_id=21')
+		test = mtafeed.ParseFromString(response.read())
 		current_time = datetime.datetime.now()
 		for stop in STOP_IDS:
 			for entity in mtafeed.entity:
-			                if entity.trip_update:
-			                    for update in entity.trip_update.stop_time_update:
-			                        if update.stop_id == stop:
-							time = update.arrival.time
-							if time <= 0:
-								time = update.departure.time
-							time = datetime.datetime.fromtimestamp(time)
-							time = math.trunc(((time - current_time).total_seconds()) / 60)
-							times.append(time)
-			times.sort()
-			for time in times:
+				if entity.trip_update:
+					for update in entity.trip_update.stop_time_update:
+						if update.stop_id == stop:
+							departure_time = update.departure.time
+							departure_time = datetime.datetime.fromtimestamp(departure_time)
+							departure_time = math.trunc(((departure_time - current_time).total_seconds()) / 60)
+							departure_times.append("%s min" % departure_time)
+			departure_times.sort()
+			for time in departure_times:
 				if time < 0:
-					times.remove(time)
-			for time in times[:NUM_TRAINS]:
-				out+=str(time)
-				out+=str(',')
-			out = out[:-1]
+					departure_times.remove(time)
+			for time in departure_times[:NUM_TRAINS]:
+				train_times+=str(time)
+				train_times+=str(',')
+			train_times = train_times[:-1]
 
-			staticimg = Image.open('staticimages/' + stop[0] + stop[3] + '.ppm')
+			# staticimg = Image.open('staticimages/' + stop[0] + stop[3] + '.ppm')
+			staticimg = Image.open('staticimages/1N.ppm')
 			draw = ImageDraw.Draw(staticimg)
-			font = ImageFont.truetype('/usr/share/fonts/truetype/droid/DroidSansFallbackFull.ttf', 12)
-			draw.text((16, 1), out,(200,200,200),font=font)
+			font = ImageFont.truetype('/Library/Fonts/Arial.ttf', 10)
+			draw.text((16, 1), train_times, fill=(255,255,255), font=font)
 			staticimg.save('dynamicimages/dynamictime.ppm')
-			times = []
-			out = ''
+			departure_times = []
+			train_times = ''
 
 			os.system('sudo ./rpi-rgb-led-matrix/examples-api-use/demo --led-chain=4 -D 1 -m 5000 dynamicimages/dynamicmtime.ppm --led-no-hardware-pulse --led-gpio-mapping=adafruit-hat')
 	except Exception:
