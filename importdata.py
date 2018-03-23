bf9e016afa8812271b0b981bd2c645510bf3b735
Sarah <sarah.mcalear@gmail.com>
2018-03-22 21:51:23 -0400

    change the layout of the board

--- importdata.py
+++ importdata.py
@@ -27,46 +27,47 @@ while True:
 							departure_time = update.departure.time
 							departure_time = datetime.datetime.fromtimestamp(departure_time)
 							departure_time = math.trunc(((departure_time - current_time).total_seconds()) / 60)
-							departure_times.append("%s min" % departure_time)
+							departure_times.append(departure_time)
 			departure_times.sort()
-			for time in departure_times:
-				if time < 0:
-					departure_times.remove(time)
-			for time in departure_times[:NUM_TRAINS]:
-				train_times.append(time)
-			train_times = train_times[:-1]
+			for departure_time in departure_times:
+				if departure_time < 0:
+					departure_times.remove(departure_time)
+			for departure_time in departure_times[:NUM_TRAINS]:
+				train_times.append(departure_time)
+			# train_times = train_times[:-1]
 
 			# staticimg = Image.open('staticimages/' + stop[0] + stop[3] + '.ppm')
-			staticimg = Image.open('staticimages/F+G-train-w-background-black.ppm')
-			draw = ImageDraw.Draw(staticimg)
-			title_font = ImageFont.truetype('/usr/share/fonts/truetype/noto/NotoMono-Regular.ttf', 9)
-			time_font = ImageFont.truetype('/usr/share/fonts/truetype/noto/NotoMono-Regular.ttf', 9)
-			min_text_font = ImageFont.truetype('/usr/share/fonts/truetype/noto/NotoMono-Regular.ttf', 7)
+		staticimg = Image.open('staticimages/F+G-train-w-background-black.ppm')
+		draw = ImageDraw.Draw(staticimg)
+		title_font = ImageFont.truetype('/usr/share/fonts/truetype/noto/NotoMono-Regular.ttf', 9)
+		time_font = ImageFont.truetype('/usr/share/fonts/truetype/noto/NotoMono-Regular.ttf', 9)
+		min_text_font = ImageFont.truetype('/usr/share/fonts/truetype/noto/NotoMono-Regular.ttf', 7)
 
-			# font = ImageFont.truetype('/Library/Fonts/Arial.ttf', 10)
+		# title_font = ImageFont.truetype('/Library/Fonts/Arial.ttf', 9)
+		# time_font = ImageFont.truetype('/Library/Fonts/Arial.ttf', 9)
+		# min_text_font = ImageFont.truetype('/Library/Fonts/Arial.ttf', 7)
+		# F train
+		# name of direction
+		draw.text((20, 4), "Manhattan", fill=(255,255,255), font=title_font)
+		# next train time
+		draw.text((95, 4), ("%s min" % train_times[0]), fill=(255,255,255), font=time_font)
+		# min
+		draw.text((120, 2), str(train_times[1]), fill=(255,255,255), font=min_text_font)
 
-			# F train
-			# name of direction
-			draw.text((20, 4), "Manhattan", fill=(255,255,255), font=title_font)
-			# next train time
-			draw.text((110, 4), train_times[0], fill=(255,255,255), font=time_font)
-			# min
-			# draw.text((100, 4), "min", fill=(255,255,255), font=min_text_font)
+		# G train
+		# name of direction
+		draw.text((20, 19), "Williamsburg", fill=(255,255,255), font=title_font)
+		# next train time
+		draw.text((95, 19), ("%s min" % train_times[0]), fill=(255,255,255), font=time_font)
+		# min
+		draw.text((120, 17), str(train_times[1]), fill=(255,255,255), font=min_text_font)
 
-			# G train
-			# name of direction
-			draw.text((20, 19), "Williamsburg", fill=(255,255,255), font=title_font)
-			# next train time
-			draw.text((110, 19), train_times[1], fill=(255,255,255), font=time_font)
-			# min
-			# draw.text((120, 19), "min", fill=(255,255,255), font=min_text_font)
+		staticimg.save('dynamicimages/dynamictime.ppm')
+		departure_times = []
+		train_times = []
 
-			staticimg.save('dynamicimages/dynamictime.ppm')
-			departure_times = []
-			train_times = []
-
-			os.system('sudo ./rpi-rgb-led-matrix/examples-api-use/demo --led-chain=4 -D 1 -m 99999999 dynamicimages/dynamictime.ppm --led-no-hardware-pulse --led-gpio-mapping=adafruit-hat')
-			time.sleeep(30)
+		os.system('sudo ./rpi-rgb-led-matrix/examples-api-use/demo --led-chain=4 -D 1 -m 99999999 dynamicimages/dynamictime.ppm --led-no-hardware-pulse --led-gpio-mapping=adafruit-hat')
+		time.sleep(30)
 
 	except Exception:
 		print traceback.format_exc()
